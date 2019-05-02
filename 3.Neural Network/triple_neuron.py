cd68d58945fd5d9426331c051646fc38a1c816b7
HONG YOUNGJAE <hyj_0804@naver.com>
2019-05-02 17:01:30 +0900

    started mnist

---
+++ 3.Neural Network/triple_neuron.py
@@ -0,0 +1,35 @@
+import numpy as np
+import sigmoid as sigmoid
+
+X = np.array([1.0, 0.5])
+W1 = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+B1 = np.array([0.1, 0.2, 0.3])
+
+print(W1.shape) #(2,3)
+print(X.shape) #(2,)
+print(B1.shape) #(3,)
+
+A1 = np.dot(X, W1) + B1
+
+Z1 = sigmoid(A1)
+
+print(A1) # [0.3, 0.7, 1.1]
+print(Z1) # [0.57444252, 0.66818777, 0.75026011]
+
+W2 = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+B2 = np.array([0.1, 0.2])
+
+print(Z1. shape) # (3,)
+print(W2.shape) # (3, 2)
+print(B2.shape) # (2, )
+
+A2 = np.dot(Z1, W2) + B2
+Z2 = sigmoid(A2)
+
+def identity_function(x):
+    return x
+W3 = np.array([[0.1, 0.3], [0.2, 0.4]])
+B3 = np.array([0.1, 0.2])
+
+A3 = np.dot(Z2, W3) + B3
+Y = identity_function(A3)
